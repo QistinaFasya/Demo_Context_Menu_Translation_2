61f49659eed34c9cad4967152c051bc67ee236b9
QistinaFasya <18015170@myrp.edu.sg>
2020-06-01 13:22:08 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id18015170/democontextmenutranslation2/AnswerActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id18015170.democontextmenutranslation2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+    }
+}
